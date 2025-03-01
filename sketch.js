a7cc74ccd0ee98c0fd731ce16f9a5d0d5f4e6827
Alex Mathers <alex.mathers@alex.mathers261f>
2025-02-28 17:02:06 -0800

    [sketch.js] 2025.02.28-17.02.06

--- sketch.js
+++ sketch.js
@@ -18,23 +18,21 @@ const sketch = () => {
 
   const margin = 2048*0.2
   const colorCount = random.rangeFloor(1, 6)
-  const palette = random.shuffle(random.pick(palettes)).slice(1, colorCount)
-
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount)
 
   const createGrid = () => {
     const points = []
-    const count = 40
+    const count = 50
     for (let x = 0; x < count; x ++) {
       for (let y = 0; y < count; y ++) {
-        // Create UV values that are between 0 & 1
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v = count <= 1 ? 0.5 : y / (count - 1)
-        const radius = Math.abs(random.noise2D(u, v) * 0.15)
+        const radius = Math.abs((random.noise2D(u, v, 1.5)) * 0.15)
         points.push({
           color: random.pick(palette),
           position: [ u, v ],
           radius,
-          rotation: random.noise2D(u, v) * 0.5
+          rotation: random.noise2D(u, v, 0.1) * 2
         });
       }
     }
@@ -42,7 +40,7 @@ const sketch = () => {
   };
 
   const points = createGrid()
-    .filter(() => random.value() > 0.07)
+    .filter(() => random.value() > 0.15)
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white'
@@ -61,17 +59,20 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u)
       const y = lerp(margin, height - margin, v)
 
+
+      context.save()
       // context.beginPath()
       // context.arc(x, y, radius * width, 0, Math.PI * 2, false)
       // context.fillStyle = color
+      // context.globalAlpha = 0.7
       // context.fill()
-      context.save()
 
       context.fillStyle = color
       context.font = `${radius * width}px "Helvetica"`
       context.translate(x, y)
       context.rotate(rotation)
-      context.fillText('=', 0, 0)
+      context.globalAlpha = 0.7
+      context.fillText('—', 0, 0)
 
       context.restore()
     });
